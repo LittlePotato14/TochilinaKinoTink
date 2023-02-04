cdf603458a8edc92727496d29f7d87ea4de5cec4
Tochilina Polina <narkada@mail.ru>
2023-02-04 11:54:34 +0300

    initial

---
+++ app/src/main/java/com/example/tinkoff_kinopoisk/presentation/views/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tinkoff_kinopoisk.presentation.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tinkoff_kinopoisk.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
